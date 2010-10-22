c71a6ceb292163229e360c83fb92b910c483f0f4
Alexander Wingård <alexander.wingard@gmail.com>
2010-10-23 01:36:19 +0200

    Use basic auth for login and some erlang.org layout.

--- site/src/index.erl
+++ site/src/index.erl
@@ -9,51 +9,25 @@ main() -> #template { file="./templates/bare.html" }.
 title() -> "Welcome to ///MTG".
 
 body() ->
-    #container_12 { body=[
-        #grid_8 { alpha=true, prefix=2, suffix=2, omega=true, body=inner_body() }
-    ]}.
+    [#panel{id = leftnav, body = search()},
+     #panel{id = content, body = content()}
+    ].
 
-inner_body() -> 
-    case wf:user() of
-        undefined ->
-            [#panel{id = loginPanel, body = login_panel()}];
-        _UserID ->
-	    wf:wire(cards2, #event{type=keyup, postback = cards2}),
-            [#panel{id = loginPanel, body = logged_in_panel()},
-	     #panel{id = wtts, body = [card(C) || C <- mtg_db:all_wtts()]},
-	     #panel{id = requestPanel, body = []}, 
-	     #textbox_autocomplete{tag = cards},
-	     #textbox{id = cards2}]
-    end.
+content() -> 
+    [#panel{id = wtts, body = [card(C) || C <- mtg_db:all_wtts()]}].
 
-login_panel() ->
-    [#textbox{id = userTextBox},
-     #password{id = passwordTextBox, postback = login},
-     #button{id = loginButton, text = "Login", postback = login},
-     #label{id = loginFault, style = "display: none;", text = "Wrong username or password"}].
+search() ->
+    [#textbox{id = search, postback = search},
+     #panel{id = searchPanel, body = []}].
 
-logged_in_panel() ->
-    [#label{text = "Welcome " ++ integer_to_list(wf:user())},
-     #button{id = logoutButton, text = "Logout", postback = logout}].
-
-event(login) ->
-    User = wf:q(userTextBox),
-    Password = wf:q(passwordTextBox),
-    
-    case mtg_db:login(User, Password) of
-        UserID when is_integer(UserID) ->
-            wf:user(UserID),
-            wf:redirect("/");
-        not_found ->
-            wf:wire(loginFault, #appear{})
-    end;
 event(logout) ->
     wf:logout(),
     wf:redirect("/");
-event(cards2) ->
-    Request = wf:q(cards2),
-    Completions = [card(C) || C <- lists:sublist(mtg_db:autocomplete_card(Request), 4)],
-    wf:update(requestPanel, Completions);
+event(search) ->
+    Request = wf:q(search),
+    Result = mtg_db:autocomplete_card(Request),
+    Completions = [card(C) || C <- lists:sublist(Result, 10)],
+    wf:update(searchPanel, [wf:f("Found ~w matching cards", [length(Result)]), Completions]);
 event({wtt, Callback, Id}) ->
     mtg_db:Callback(Id, wf:user()),
     wf:replace(Id, card(mtg_db:get_card(Id))).
@@ -68,6 +42,7 @@ card(Card) ->
 	       false ->
 		   #link{text = "Want", postback = {wtt, add_wanter, Id}}
 	   end,
+	   "&nbsp;",
 	   case Have of 
 	       true ->
 		   #link{text = "Don't have", postback = {wtt, del_haver, Id}};
@@ -81,14 +56,3 @@ card(Card) ->
 		   Wtt
 		  ]
 	  }.
-
-autocomplete_enter_event(SearchTerm, cards) ->
-    List = [{struct,[{id, list_to_binary(C#cards.id) }, 
-		     {label, list_to_binary(C#cards.name)}, 
-		     {value, list_to_binary(C#cards.name)}]} || 
-	       C <- mtg_db:autocomplete_card(SearchTerm)],
-    mochijson2:encode(List).
-
-autocomplete_select_event({struct, [{<<"id">>, Id },{<<"value">>, Value}]} , cards) ->
-    wf:update(requestPanel, [#image{image = "http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=" ++ binary_to_list(Id) ++ "&type=card"}]),
-    ok.
