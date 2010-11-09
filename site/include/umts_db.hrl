f0e79f16fff5daa82945023f26b353f879836541
Alexander Wingård <alexander.wingard@gmail.com>
2010-11-09 23:57:33 +0100

    Update users table to contain displayname

--- site/include/umts_db.hrl
+++ site/include/umts_db.hrl
@@ -1,4 +1,4 @@
--record(users, {id, name, password}).
+-record(users, {id, name, password, display}).
 -record(wtts, {id, wanters = ordsets:new(), havers = ordsets:new()}).
 -record(auto_increment, {table, key}).
 -record(cards, {id, name}).
