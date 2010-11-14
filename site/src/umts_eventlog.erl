5487967195375912c1a96a6bc0438624f282432a
Alexander Wingård <alexander.wingard@gmail.com>
2010-11-15 00:06:28 +0100

    Add event listing page

--- site/src/umts_eventlog.erl
+++ site/src/umts_eventlog.erl
@@ -1,5 +1,7 @@
 -module(umts_eventlog).
 -compile(export_all).
+-include_lib("nitrogen/include/wf.hrl").
+
 -include("umts_db.hrl").
 
 log_login(User) ->
@@ -16,3 +18,33 @@ log_event(Event) ->
 		mnesia:write(#events{time = now(), event = Event})
 	end,
     {atomic, ok} = mnesia:transaction(T).
+
+get_events() ->
+    Items = lists:flatmap(fun format_event/1, lists:reverse(lists:keysort(#events.time, umts_db:all(events)))),
+    #list{body = Items}.
+
+format_event(Event) ->
+    case format_event_event(Event#events.event) of
+	not_ok ->
+	    [];
+	Formated ->
+	    [#listitem{text = wf:f("~w: ~s", [calendar:now_to_local_time(Event#events.time),
+					      Formated])}]
+    end.
+
+format_event_event(#wtt_event{user = UserID, card = CardID, kind = Kind}) ->
+    User = umts_db:get_user(UserID),
+    Card = umts_db:get_card(CardID),
+    Action = case Kind of 
+		 add_wanter ->
+		     "now wants";
+		 del_wanter ->
+		     "no longer wants";
+		 add_haver ->
+		     "now haves";
+		 del_haver ->
+		     "no longer haves"
+	     end,
+    wf:f("~s ~s ~s", [User#users.name, Action, Card#cards.name]);
+format_event_event(Event) ->
+    not_ok.
